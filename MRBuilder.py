0ada694752f0ecc11ef538e84fda5c17ebdbf4ce
sharanya <sharanyakv96@gmail.com>
2017-04-10 11:20:59 +0530

    Code Modified

---
+++ MRBuilder.py
@@ -0,0 +1,19 @@
+import nltk
+
+def generateMR():
+    nlq = input('Natural language query : ')
+    text = nltk.word_tokenize(nlq)
+    nlquery = nltk.pos_tag(text)
+
+    print(nlquery)
+
+    keywords = []
+    numerals = []
+    tags = ['NN', 'NNS', 'NNP', 'NNPS', 'JJ']
+
+    for i in nlquery:
+        if i[1] in tags:
+            keywords.append(i[0])
+        elif i[1] == 'CD':
+            numerals.append(i[0])
+    return keywords,text, nlq, numerals
